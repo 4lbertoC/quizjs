034a34810a7847e178fcb1691526c1df57585709
Alberto Congiu <4lbertoC@gmail.com>
2015-02-22 19:09:22 +0100

    reverted origin

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ io.sockets.on('connection', function(socket) {
 });
 
 app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', "albertocongiu.com");
+	res.setHeader('Access-Control-Allow-Origin', "*");
 	res.setHeader('Access-Control-Allow-Credentials', "true");
 
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
