66192b6bae79a368657f20382c8a129eb84ae735
Alberto Congiu <4lbertoC@gmail.com>
2015-02-23 09:33:31 +0100

    removed cors setting

--- index.js
+++ index.js
@@ -110,15 +110,6 @@ io.sockets.on('connection', function(socket) {
 
 });
 
-app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', "*");
-	res.setHeader('Access-Control-Allow-Credentials', "true");
-
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-	next();
-});
-
 app.set('port', (process.env.PORT || DEFAULT_PORT))
 
 module.exports = function() {
